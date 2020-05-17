2da9dea66a60db4e8b2657d8e54591cb8bf265b6
tyobigoro_i <tyobigorox@gmail.com>
2020-05-17 19:02:46 +0900

    c

--- tScrollView/ViewController.swift
+++ tScrollView/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   let scrollView = UIScrollView()
+    
+    var strs = ["A", "B", "C", "D", "E"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        scrollView.frame = self.view.frame
+        scrollView.backgroundColor = UIColor.red
+        scrollView.contentSize = CGSize(width: self.view.bounds.width * CGFloat(strs.count) , height: self.view.bounds.height)
+        self.view.addSubview(scrollView)
+        
+        //ページングを有効にする
+        scrollView.isPagingEnabled = true
 
+        for i in strs.enumerated() {
+            
+            let label = UILabel(frame: CGRect(x: CGFloat(i.offset)*view.bounds.size.width,
+                                              y: 0,
+                                              width: view.bounds.size.width,
+                                              height: view.bounds.size.height))
+            label.textAlignment = .center
+            label.backgroundColor = .blue
+            label.text = i.element
+            
+            scrollView.addSubview(label)
+        }
+    }
 
 }
 
